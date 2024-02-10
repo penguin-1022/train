fc33c5b29c5704daa85a9d9b206690fdf4ee62d6
penguin1022@126.com <penguin1022@126.com>
2024-02-10 17:12:51 +0800

    修改启动日志，添加/member前缀，修改logback日志

--- member/src/main/java/com/jl/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jl/train/member/config/MemberApplication.java
@@ -1,14 +1,21 @@
 package com.jl.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 //不能只扫描com。因为只扫描com的话，会把第三方jar包也扫描上，会出问题
 @ComponentScan("com.jl")
 public class MemberApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("地址：\thttp://localhost:{}", env.getProperty("server.port"));
     }
 }
