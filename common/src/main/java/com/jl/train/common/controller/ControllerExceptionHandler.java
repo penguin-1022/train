83fb5a59b06e86d312b5de231f009da6607cb4c8
penguin1022@126.com <penguin1022@126.com>
2024-02-11 23:23:11 +0800

    4.17 为项目添加统一异常处理

---
+++ common/src/main/java/com/jl/train/common/controller/ControllerExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.jl.train.common.controller;
+
+import com.jl.train.common.resp.CommonResp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//统一异常处理
+//相当于是一个拦截器，对发生异常的，进行拦截处理
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
+//    所有异常统一处理
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(Exception e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("系统异常", e);
+        commonResp.setSuccess(false);
+        commonResp.setMessage("系统出现异常，请联系管理员");
+        return commonResp;
+    }
+
+//    业务异常统一处理
+//    @ExceptionHandler(value = BusinessException.class)
+//    @ResponseBody
+//    public CommonResp exceptionHandler(BusinessException e){
+//        CommonResp commonResp = new CommonResp();
+//        LOG.error("系统异常：", e.getE().getDesc());
+//        commonResp.setSuccess(false);
+//        commonResp.setMessage(e.getE().getDesc());
+//        return commonResp;
+//    }
+
+//    校验异常统一处理
+    @ExceptionHandler(value = BindException.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(BindException e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("校验异常：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        return commonResp;
+    }
+}
