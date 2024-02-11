81411cde62af7c8f7033c10462c37db91687c1b0
penguin1022@126.com <penguin1022@126.com>
2024-02-11 22:10:20 +0800

    4.15 实现一个简单的注册接口，使用generator生成的代码

--- member/src/main/java/com/jl/train/member/controller/MemberController.java
+++ member/src/main/java/com/jl/train/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.jl.train.member.controller;
 
 import com.jl.train.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,12 @@ public class MemberController {
     private MemberService memberService;
 
     @RequestMapping(method = RequestMethod.GET, value = "/count")
-    public Integer hello() {
+    public Integer count() {
         return memberService.count();
+    }
+
+    @PostMapping("/register")
+    public Long register(String mobile) {
+        return memberService.register(mobile);
     }
 }
--- member/src/main/java/com/jl/train/member/service/MemberService.java
+++ member/src/main/java/com/jl/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.jl.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.jl.train.member.domain.Member;
+import com.jl.train.member.domain.MemberExample;
 import com.jl.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -10,5 +15,26 @@ public class MemberService {
     private MemberMapper memberMapper;
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+//    根据用户填写的手机号，实现新用户注册
+    public long register(String mobile) {
+//        先进行一下条件查询，判断是否已经注册过了
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+//        根据创建好的条件，进行条件查询
+        List<Member> list = memberMapper.selectByExample(memberExample);
+
+//        返回的结果不是null，说明已经注册过了，那么报个错就结束了
+        if (CollUtil.isNotEmpty(list)) {
+//            return list.get(0).getId();
+            throw new RuntimeException("手机号已经注册！");
+        }
+//        开始进行新用户创建，并写入到数据库中
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
