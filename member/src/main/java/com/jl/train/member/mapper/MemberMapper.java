f027b60155f3fb3ecba4ca32afbc2732cd41b3fc
penguin1022@126.com <penguin1022@126.com>
2024-02-11 21:32:07 +0800

    4.14 集成mybatis代码生成器

--- member/src/main/java/com/jl/train/member/mapper/MemberMapper.java
+++ member/src/main/java/com/jl/train/member/mapper/MemberMapper.java
@@ -1,5 +1,33 @@
 package com.jl.train.member.mapper;
 
+import com.jl.train.member.domain.Member;
+import com.jl.train.member.domain.MemberExample;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
 public interface MemberMapper {
-    int count();
+    long countByExample(MemberExample example);
+
+    int deleteByExample(MemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member record);
+
+    int insertSelective(Member record);
+
+    List<Member> selectByExample(MemberExample example);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByExample(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByPrimaryKeySelective(Member record);
+
+    int updateByPrimaryKey(Member record);
 }
