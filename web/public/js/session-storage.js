bccd9868fd1da2378b82c0d527d39ffc06438b71
penguin1022@126.com <penguin1022@126.com>
2024-02-14 16:49:04 +0800

    6.6 vuex配合h5解决浏览器刷新丢失数据的问题

---
+++ web/public/js/session-storage.js
@@ -0,0 +1,20 @@
+SESSION_ORDER = "SESSION_ORDER";
+SESSION_TICKET_PARAMS = "SESSION_TICKET_PARAMS";
+
+SessionStorage = {
+    get: function (key) {
+        let v = sessionStorage.getItem(key);
+        if (v && typeof(v) !== "undefined" && v !== "undefined") {
+            return JSON.parse(v);
+        }
+    },
+    set: function (key, data) {
+        sessionStorage.setItem(key, JSON.stringify(data));
+    },
+    remove: function (key) {
+        sessionStorage.removeItem(key);
+    },
+    clearAll: function () {
+        sessionStorage.clear();
+    }
+};
--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,14 +1,18 @@
 import { createStore } from 'vuex'
 
+const MEMBER = "MEMBER";
+
 export default createStore({
   state: {
-    member: {}
+    // || {}是前端用来避免空指针异常的一种写法
+    member: window.SessionStorage.get(MEMBER) || {}
   },
   getters: {
   },
   mutations: {
     setMember (state, _member) {
       state.member = _member;
+      window.SessionStorage.set(MEMBER, _member);
     }
   },
   actions: {
