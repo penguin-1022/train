416f25d35c03df1404bda71c97ab80622aebb24a
penguin1022@126.com <penguin1022@126.com>
2024-02-15 12:25:03 +0800

    7.6 制作欢迎页面，绑定侧边栏和导航栏

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -17,6 +17,10 @@ const routes = [
       {
         path: 'welcome',
         component: () => import('../views/main/WelcomeView.vue'),
+      },
+      {
+        path: 'passenger',
+        component: () => import('../views/main/PassengerView.vue')
       }
     ]
   },
